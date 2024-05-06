59488df4ca14af4d2f1adee82e5b1205ac17103c
himelmia535 <himelmia535@gmail.com>
2024-05-06 20:18:18 +0600

    firebase setup

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqg_11t4SHxT7KwvsDC-YelSiDrqsQ3bo",
+  authDomain: "jobfinity-986c6.firebaseapp.com",
+  projectId: "jobfinity-986c6",
+  storageBucket: "jobfinity-986c6.appspot.com",
+  messagingSenderId: "221678111201",
+  appId: "1:221678111201:web:182f78e0d32db714a8aaa0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
